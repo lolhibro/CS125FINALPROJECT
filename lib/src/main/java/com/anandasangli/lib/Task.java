64dac6acf36c8be509174aaa5e2ed285fa372a75
Ananda Sangli <asangli2@ilinois.edu>
2019-04-30 00:36:07 -0500

    Tasks Object Definition

---
+++ lib/src/main/java/com/anandasangli/lib/Task.java
@@ -0,0 +1,47 @@
+package com.anandasangli.lib;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Task {
+    public final int MIN_PRIORITYLVL = 1;
+    public final int MAX_PRIORITYLVL = 5;
+
+    private String name;
+    private int priorityLvl;
+    private int completionHours;
+    private Date dueDate;
+    Date currentDate = new Date();
+    long dayDiff = TimeUnit.DAYS.convert((dueDate.getTime() - currentDate.getTime()), TimeUnit.MILLISECONDS);
+
+    public void setName(String setName) {
+        name = setName;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setPriorityLvl(int setPriorityLvl) {
+        if (priorityLvl >= MIN_PRIORITYLVL && priorityLvl <= MAX_PRIORITYLVL) {
+            priorityLvl = setPriorityLvl;
+        }
+    }
+    public int getPriorityLvl() {
+        return priorityLvl;
+    }
+    public void setCompletionHours(int setCompletionHours) {
+        completionHours = setCompletionHours;
+    }
+    public int getCompletionHours() {
+        return completionHours;
+    }
+    public void setDueDate(Date setDueDate) {
+        dueDate = setDueDate;
+    }
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public long getDayDiff() {
+        return dayDiff;
+    }
+}
